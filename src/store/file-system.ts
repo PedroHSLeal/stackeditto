664f031528833b81eb0d93b4735d817ef8f18697
Pedro Leal <henrique.santos.leal.99@gmail.com>
2025-12-08 19:00:02 -0300

    remocao codigo morto no pinia store

--- src/store/file-system.ts
+++ src/store/file-system.ts
@@ -1,40 +1,20 @@
 
-import { type CustomDirectory, type CustomFile } from "@/models/file-system";
+import { type CustomDirectory } from "@/models/file-system";
 import { defineStore } from "pinia";
 
 export type StoreState = {
-  originalHandler: FileSystemDirectoryHandle | null;
+  originalHandler: unknown;
 
   directory: CustomDirectory | null;
   configDirectory: CustomDirectory | null;
-
-  // por agora, nao acesse o $state.openedFiles, utilize os actions do store
-  // openedFiles: { [key: string]: DiffResult };
-
-  // recentlyOpenedFile: string;
 }
-
-// type FileDiff = StoreState['openedFiles'][keyof StoreState['openedFiles']];
-
-export type Nullable<T> = { [K in keyof T]: T[K] | null };
 
 export const useFileSystemStore = defineStore('file-system', {
   state: (): StoreState => ({
     originalHandler: null,
+
     directory: null,
     configDirectory: null,
-    // openedFiles: {},
-    // recentlyOpenedFile: ""
   }),
-  actions: {
-    /* openFile(filePath: string, diff: FileDiff): void {
-      this.openedFiles[filePath] = diff;
-    }, */
-    /* getFile(filePath: string): FileDiff {
-      return this.openedFiles[filePath];
-    }, */
-    /* hasOpenFile(filePath: string): boolean {
-      return Object.keys(this.openedFiles).includes(filePath);
-    } */
-  }
+  actions: {}
 })
