c73b3573dd577878aeb5779de5cf76c09c9aa27f
Pedro Leal <henrique.santos.leal.99@gmail.com>
2026-01-05 18:16:34 -0300

    ajuste nos testes

--- src/services/file-system/fsa/index.spec.ts
+++ src/services/file-system/fsa/index.spec.ts
@@ -1,69 +1,41 @@
 import { beforeEach, describe, expect, it, vi } from "vitest";
-import { nodeToFsa } from "memfs/lib/node-to-fsa";
-import { fs, memfs, vol } from "memfs";
 import { useFileSystem } from ".";
+import { useFileSystem as useInMemoryFileSystem } from "../in-memory";
 import { EXTENSION_STRUCTURE } from "@/services/untrusted-code-extensions";
+import type { Structure } from "../in-memory/json-structure";
 
 describe("file-system service suite", async () => {
-  // eu nao sei se esse tipo de teste é realmente necessário, porém estou fazendo para caso mais de estudo
-  // atualizacao 05/05: nao tá fazendo muito sentido esses testes, tenho que testar outras alternativas
-  describe("openDirectory()", async () => {
-    it("should return the chosen directory by the user", async () => {
-      vi.stubGlobal("showDirectoryPicker", vi.fn(() => {
-        return nodeToFsa(fs, "/root", { mode: "readwrite" })
-      }));
-
-      const { openDirectory } = useFileSystem();
-
-      const v = await openDirectory();
-
-      expect(v).toBeDefined();
-      expect(v?.name).toBe("root")
-      expect(v?.kind).toBe("directory");
-    });
-    it("should catch the error for whatever reason", async () => {
-      vi.stubGlobal("showDirectoryPicker", vi.fn(() => {
-        throw new DOMException("DEU RUIM!", "AbortError")
-      }));
-
-      const spy = vi.spyOn(console, "error")
-
-      const v = await useFileSystem().openDirectory();
-
-      expect(v).toBeUndefined();
-      expect(spy).toBeCalled();
-    });
-  });
-
   describe("buildDirectoryStructure()", async () => {
-    it("should return a representation of the chosen directory", async () => {
-      const { fs: localFs } = memfs({
-        "folder-1": {
-          "file-1.md": "Hello World"
-        }
-      });
-      const expectCustomDirectory = {
-        directories: [
+    it.only("should return a representation of the chosen directory", async () => {
+      const structure: Structure = {
+        name: "folder-1",
+        kind: "directory",
+        children: [
           {
-            webkitRelativePath: "/folder-1",
-            files: [
-              {
-                webkitRelativePath: "/folder-1/file-1.md"
-              }
-            ]
+            kind: "file",
+            name: "file-1.md",
+            content: ""
           }
         ]
-      }
+      };
 
-      // como a tipagem do memfs é diferente do metodo do buildDirectoryStructure, estou forçando a tipagem do handler para "any"
-      const handler: any = nodeToFsa(localFs, "", { mode: "readwrite" });
+      const handler = await useInMemoryFileSystem().openDirectory(structure);
+
+      const expectCustomDirectory = {
+        webkitRelativePath: "folder-1",
+        files: [
+          {
+            webkitRelativePath: "folder-1/file-1.md"
+          }
+        ]
+      };
 
       const { buildDirectoryStructure } = useFileSystem();
 
       const customDirectory = await buildDirectoryStructure(handler);
 
-      expect(customDirectory?.directories[0].webkitRelativePath).toBe(expectCustomDirectory.directories[0].webkitRelativePath);
-      expect(customDirectory?.directories[0].files[0].webkitRelativePath).toBe(expectCustomDirectory.directories[0].files[0].webkitRelativePath);
+      expect(customDirectory?.webkitRelativePath).toBe(expectCustomDirectory.webkitRelativePath);
+      expect(customDirectory?.files[0].webkitRelativePath).toBe(expectCustomDirectory.files[0].webkitRelativePath);
     });
     it("should return undefined if the handler is undefined", async () => {
       const { buildDirectoryStructure } = useFileSystem();
@@ -71,32 +43,6 @@ describe("file-system service suite", async () => {
       const customDirectory = await buildDirectoryStructure(undefined);
 
       expect(customDirectory).toBeUndefined();
-    });
-  });
-
-  describe("populateDirectory()", async () => {
-    it("should return a object containing the proper chosen directory and the app directory, containing config file and extensions", async () => {
-      const filesInDirectory: any = {
-        directories: [
-          {
-            webkitRelativePath: "seila",
-            directories: [],
-            files: []
-          },
-          {
-            webkitRelativePath: EXTENSION_STRUCTURE.EXTENSION_FOLDER,
-            directories: [],
-            files: []
-          },
-        ]
-      }
-
-      const { populateDirectory } = useFileSystem();
-
-      const customDirectory = populateDirectory(filesInDirectory);
-
-      expect(customDirectory?.directory?.directories).toHaveLength(1);
-      expect(customDirectory?.applicationDirectory).toBeDefined();
     });
   });
 });
--- src/services/file-system/index.ts
+++ src/services/file-system/index.ts
@@ -1,16 +1,16 @@
 import { useFileSystem as fsaUseFileSystem } from "./fsa";
-// import { useFileSystem as inMemoryFileSystem } from "./in-memory";
+import { useFileSystem as inMemoryFileSystem } from "./in-memory";
 
 type Implementations = {
   physical: ReturnType<typeof fsaUseFileSystem>;
-  // inMemory: ReturnType<typeof inMemoryFileSystem>;
+  inMemory: ReturnType<typeof inMemoryFileSystem>;
 }
 
-// export function useFileSystem(implementation?: "physical"): ReturnType<typeof fsaUseFileSystem>;
-// export function useFileSystem(implementation?: "inMemory"): ReturnType<typeof inMemoryFileSystem>;
+export function useFileSystem(implementation?: "physical"): ReturnType<typeof fsaUseFileSystem>;
+export function useFileSystem(implementation?: "inMemory"): ReturnType<typeof inMemoryFileSystem>;
 // export function useFileSystem(implementation: string = "physical") {
 export function useFileSystem<TImpl extends keyof Implementations = "physical">(implementation: TImpl): Implementations[TImpl] | undefined {
   if (implementation == "physical") return fsaUseFileSystem() as Implementations[TImpl];
-  // else if (implementation == "inMemory") return inMemoryFileSystem() as Implementations[TImpl];
+  else if (implementation == "inMemory") return inMemoryFileSystem() as Implementations[TImpl];
   else return;
 }
