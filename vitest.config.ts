71a47046c09ccea0ed5e741331c327b64bfb2ef0
Pedro Leal <henrique.santos.leal.99@gmail.com>
2026-01-05 20:27:11 -0300

    troca de ambiente de jsdom para happy-dom

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,7 +6,7 @@ export default mergeConfig(
   viteConfig({ mode: "", command: "serve" }),
   defineConfig({
     test: {
-      environment: 'jsdom',
+      environment: 'happy-dom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
     },
